32b192063722e6d71d0593514563782228e90063
Abhay Patel <abhay.patel@rocketmail.com>
2021-05-08 00:46:35 -0400

    finished project

--- MarsInTheRoom/ViewController.swift
+++ MarsInTheRoom/ViewController.swift
@@ -23,10 +23,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let sphere = SCNSphere(radius: 0.5)
+        
+        
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "art.scnassets/mars.jpeg")
+        
+        sphere.materials = [material]
+        
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x:0,y:0, z:-1)
+        
+        
+        node.geometry = sphere
+        
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
